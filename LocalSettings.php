c23923459ef651330aebb0102659c7d4bbdd01f2
AntiJawn <41398876+AntiJawn@users.noreply.github.com>
2020-08-29 10:55:35 -0700

    Update LocalSettings.php

--- LocalSettings.php
+++ LocalSettings.php
@@ -118,7 +118,7 @@ $wgDiff3 = "";
 
 ## Default skin: you can change the default skin. Use the internal symbolic
 ## names, ie 'vector', 'monobook':
-$wgDefaultSkin = "vector";
+$wgDefaultSkin = "monobook";
 
 # Enabled skins.
 # The following skins were automatically enabled:
